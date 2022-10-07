e86a12ba9a377db798a09fcf0a9019862e540ed7
Richard Marquez <richard.marquez@ryerson.ca>
2022-10-07 18:11:40 -0400

    i need help a lifeline anything

--- question3_highest_cost_path.pl
+++ question3_highest_cost_path.pl
@@ -14,22 +14,46 @@
 %%%%% Helper Programs
 % Put any helper programs in the space below
 
-% dfs(+tree, +CurrentCost, +CurrentList, -Highest Cost, -Highest List)
-dfs(none, 0, [], 0, []).
-dfs(tree3(X, LC, L, _, _, _, _), Cost, [X|A], HC, HL) 
-        :- dfs(L, NewCost, A, HC, HL), Cost is NewCost + LC, checkMax(Cost, A, HC, HC, HL).
-dfs(tree3(X, _, _, MC, M, _, _), Cost, [X|A], HC, HL) 
-        :- dfs(M, NewCost, A, HC, HL), Cost is NewCost + MC, checkMax(Cost, A, HC, HC, HL).
-dfs(tree3(X, _, _, _, _, RC, R), Cost, [X|A], HC, HL) 
-        :- dfs(R, NewCost, A, HC, HL), Cost is NewCost + RC, checkMax(Cost, A, HC, HC, HL).
+% THIS CODE IS CURRENTLY ABLE TO OUTPUT ALL COSTS AND PATHS, YOU JUST NEED TO IMPLEMENT A WAY TO GET THE MAX OF THEM
 
-checkMax(Cost, A, CVal, Cost, HL) :- Cost >= CVal.
-checkMax(Cost, A, CVal, HC, HL) :- Cost < CVal.
+% dfs(+tree, +CurrentCost, +CurrentList, -Highest Cost, -Highest List)
+
+% Base Case 1: CurrentList is empty, so CurrentCost becomes empty and the HighestCost/HighestList are 0 and [] respectively.
+% Note this is only used for testing purposes (ie. empty tree), in an input that has more than one node, this case shouldn't be hit.
+dfs(none, 0, [], 0, []).
+
+% ----
+% Base Case 2: Reached terminal node, compare current cost and current list to HC and HL
+
+% Base Case 2.1: After reaching a terminal node, the cost of the path is lower than the highest. Do not update value, return previous original.
+dfs(tree3(X, _, none, _, none, _, none), Cost, _, HC, _) :- Cost < HC.
+
+% Base Case 2.2: 
+dfs(tree3(X, _, none, _, none, _, none), Cost, [X|A], HC, _) :- Cost >= HC, dfs(none, _, _, Cost, [X|A]).
+
+% Recursive Clause 1: Recurse through the left subtree
+dfs(tree3(X, LC, L, _, _, _, _), Cost, [X|A], HC, HL) 
+        :- dfs(L, NewCost, A, HC, HL), Cost is NewCost + LC.
+
+% Recursive Clause 2: Recurse through the middle subtree
+dfs(tree3(X, _, _, MC, M, _, _), Cost, [X|A], HC, HL) 
+        :- dfs(M, NewCost, A, HC, HL), Cost is NewCost + MC.
+
+% Recursive Clause 3: Recurse through the right subtree
+dfs(tree3(X, _, _, _, _, RC, R), Cost, [X|A], HC, HL) 
+        :- dfs(R, NewCost, A, HC, HL), Cost is NewCost + RC.
+
+
+
+% Clause 1: Current Cost is less than the CVal, nothing changes.
+% checkMax(CurrentCost, CurrentList, HighestCost, HighestList) :- CurrentCost < HighestCost.
+
+% Clause 2: Current cost is greater than HighestCost, call checkMax again with the updated values.
+% checkMax(CurrentCost, CurrentList, HighestCost, CurrentList) :- CurrentCost >= HighestCost, checkMax(_, _, CurrentCost, CurrentList).
 
 %%%%% RULE: highestCostPath
 % Add the rule(s) for highestCostPath below
-highestCostPath(T, PC, PL) :- dfs(T, _, _, PC, PL).
-
+highestCostPath(T, PC, PL) :- 
 %%%%% TESTS
 % Below is a test tree, based on the diagram in the assignment
 % You can use it in a query as follows:
--- question4_merge_sort.pl
+++ question4_merge_sort.pl
@@ -13,15 +13,22 @@
 
 %%%%% RULE: listLength
 % Add the rule(s) for listLength below
-
+listLength(empty_list, 0).
+listLength(listTerm(_, T), Length) :- listLength(T, NewLength), Length is NewLength + 1.
 
 %%%%% RULE: divideList
 % Add the rule(s) for divideList below
-
+divideList(L, 0, empty_list, L).
+divideList(listTerm(H, T), N, listTerm(H, F), L) :- divideList(T, N1, F, L), N is N1 + 1.
 
 %%%%% RULE: mergeSortedLists
 % Add the rule(s) for mergeSortedLists below
 
+% listTerm(4, listTerm(6, listTerm(8, listTerm(10, empty_list)))), listTerm(3, listTerm(5, listTerm(7, listTerm(9, empty_list))))
+mergeSortedLists(empty_list, L, L). 
+mergeSortedLists(L, empty_list, L).
+mergeSortedLists(listTerm(H1, T1), listTerm(H2, T2), listTerm(H1, L)) :- H1 =< H2, mergeSortedLists(T1, listTerm(H2, T2), L).
+mergeSortedLists(listTerm(H1, T1), listTerm(H2, T2), listTerm(H2, L)) :- H2 < H1, mergeSortedLists(listTerm(H1, T1), T2, L).
 
 %%%%% Helpers
 % Add any additional helpers below
@@ -29,7 +36,41 @@
 
 %%%%% RULE: mergeSort
 % Add the rule(s) for mergeSort below
+%  python version
+% source: https://developer.nvidia.com/blog/merge-sort-explained-a-data-scientists-algorithm-guide/#:~:text=In%20the%20context%20of%20sorting,further%20to%20their%20smaller%20parts.
+% def merge_sort(list: [int]):
+%     list_length = len(list)
+    
+%     if list_length == 1:
+%         return list
+    
+%     mid_point = list_length // 2
+    
+%     left_half = merge_sort(list[:mid_point])
+%     right_half = merge_sort(list[mid_point:])
+    
+%     return merge(left_half, right_half)
+% mergeSort(ListTerm, SortedListTerm) :- listLength(ListTerm, Length), Mid is Length // 2, divideList(ListTerm, Mid, LeftList, RightList), 
+        
+% mergeSortedLists(mergeSort)
+% mergeSort(ListTerm, SortedListTerm) 
 
+% merge(LeftList, RightList, MergedList) 
+
+
+
+mergeSort(empty_list, empty_list).
+mergeSort(ListTerm, SortedListTerm) :-
+    % list_length = len(list), mid_point == list_length // 2
+
+    % get left and right list
+    divideList(ListTerm, Mid, LeftList, RightList), 
+
+    % % left_half = merge_sort(..) , right_half = merge_sort(...)
+    % mergeSort(LeftList, LeftHalf), mergeSort(RightList, RightHalf),
+
+    % return merge(left_half, right_half)
+    mergeSortedLists(mergeSort(LeftHalf), mergeSort(RightHalf), SortedListTerm).
 
 %%%%% END
 % DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
